e86351175af8da0b5a3d957be85c4456a447fa65
kukabuu <kukabuu@gmail.com>
2020-01-12 01:02:03 +0300

    add the main site layout, anchors to the location and the files on the main page, the Card block and js to animate this, the map via yandex.card api

--- src/js/aboutPage.js
+++ src/js/aboutPage.js
@@ -1,3 +1,70 @@
-import '../scss/aboutPage.scss';
+import '../scss/style.scss';
+import pin from '../assets/img/pin.svg';
 
-console.log("let's start");
+ymaps.ready(init);
+function init(ymaps) {
+  const myMap = new ymaps.Map('map', {
+    center: [64.529109, 40.566819],
+    zoom: 16,
+    controls: [],
+  });
+
+  const MyBalloonLayout = ymaps.templateLayoutFactory.createClass(
+    '<div class="popover"></div>', {
+      build() {
+        this.constructor.superclass.build.call(this);
+
+        this._$element = $('.popover', this.getParentElement());
+        // this.applyElementOffset();
+      },
+
+      // applyElementOffset() {
+      //   this._$element.css({
+      //     left: -(this._$element[0].offsetWidth / 2),
+      //     top: -(this._$element[0].offsetHeight / 2),
+      //   });
+      // },
+
+      _isElement(element) {
+        return element && element[0];
+      },
+    },
+  );
+
+  const myPlacemark = new ymaps.Placemark(
+    myMap.getCenter(), {
+      balloonContent: '',
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: pin,
+      iconImageSize: [28, 37],
+      iconImageOffset: [-20, -20],
+      hideIconOnBalloonOpen: false,
+      balloonLayout: MyBalloonLayout,
+      balloonOffset: [-80, -150],
+    },
+  );
+
+  myMap.behaviors
+    .disable(['drag', 'rightMouseButtonMagnifier']);
+  myMap.controls.add('zoomControl', {
+    position: {
+      left: 'auto',
+      right: 10,
+      bottom: 'auto',
+      top: 140,
+    },
+    size: 'small',
+  });
+
+  myMap.controls.add('geolocationControl', {
+    position: {
+      left: 'auto',
+      right: 10,
+      bottom: 'auto',
+      top: 218,
+    },
+  });
+  myMap.geoObjects.add(myPlacemark);
+  myPlacemark.ballon.open();
+}
